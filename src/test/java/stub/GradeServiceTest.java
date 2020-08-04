e4bb4a14ebed2ed2858893e9209dfaa7917fa9fd
twodogbeta <qijinhaoup@163.com>
2020-08-04 22:36:38 +0800

    finish stub

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,6 +1,11 @@
 package stub;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,7 +13,7 @@ import java.util.List;
 
 public class GradeServiceTest {
 
-    public static class MockGradeSystem extends GradeSystem{
+/*    public static class MockGradeSystem extends GradeSystem{
 
        public  List<Double> gradesFor(long studentId) {
              return Arrays.asList(80.0, 90.0, 100.0);
@@ -20,5 +25,17 @@ public class GradeServiceTest {
         GradeService gradeService = new GradeService(new MockGradeSystem());
         double result = gradeService.calculateAverageGrades(1L);
         assertEquals(90.0,result);
-    }
+    }*/
+@BeforeEach
+public void setUp() {
+}
+@Test
+void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100ByMockito() {
+    GradeSystem gradeSystem = mock(GradeSystem.class);
+    Mockito.when(gradeSystem.gradesFor(1L)).thenReturn( Arrays.asList(80.0, 90.0, 100.0));
+    //Assertions.assertEquals(90.0, result);
+    GradeService gradeService = new GradeService(gradeSystem);
+    double result = gradeService.calculateAverageGrades(1L);
+    assertEquals(90.0,result);
+}
 }
